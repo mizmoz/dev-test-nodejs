1a402416682e95e441ac6ebf84ab59bf7c643274
TheRealVish <vish@goldsmithsconsulting.com>
2020-01-16 00:14:59 +0000

    ts not json country list api tsNotJsonCountries

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("Hello and good luck!");
+'use strict';
+const cc = require('./api/country')
+const express = require('express')
+var bodyParser = require('body-parser');
+const app = express()
+const port = 3000
+
+app.get('/tsNotJsonCountries', (request,response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.send(cc.tsNotJson());
+});
+app.get('/countries', (request,response) => {
+    response.setHeader('Content-Type', 'application/json');
+    response.send(cc.countryList());
+});
+app.use(bodyParser.json());
+app.post('/addcountry', (request, response) => {
+    response.setHeader('Content-Type', 'application/json');
+    response.send(cc.nowAddCountry(request.body));
+});
+app.get('/authList/:user/:password', (request,response) => {
+    var usr = request.params.user;
+    var pwd = request.params.password;
+    response.setHeader('Content-Type', 'application/json');
+    response.send(cc.authList(usr,pwd));  
+});
+app.listen(port, (err:any) => {
+  if (err) {
+    return console.log('not cool!', err)
+  }
+  console.log(`yep on ${port}`)
+})
