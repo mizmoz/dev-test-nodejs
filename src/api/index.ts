7662cbe0dd9053c108c9271673e8e018c847b537
John Dave Decano <johndavedecano@gmail.com>
2020-06-22 06:49:18 +0800

    added seeder

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,10 +4,14 @@ import auth from './auth'
 
 import country from './country'
 
+import seeder from './seeder'
+
 const router: express.Router = express.Router()
 
 router.use('/auth', auth)
 
 router.use('/country', country)
+
+router.post('/seeder', seeder)
 
 export default router
