8890e62a43f97fd4f954ea23fd5888136bebbe57
Patrick Buitre <patrickbuitre@Pats.local>
2020-06-22 16:32:29 +0800

    Added host and port config

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ require('dotenv-safe').config()
 
 import { Server } from "@hapi/hapi";
 import loadPlugins from './pluglins';
+const { API_PORT, API_HOST } = process.env;
 
 const init = async () => {
   const server: Server = new Server({
-    port: 3000,
-    host: 'localhost'
+    host: API_HOST || '127.0.0.1',
+    port: API_PORT || 3000,
   });
 
   await loadPlugins(server);
