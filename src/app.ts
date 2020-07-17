09aa6ea71181b132f9fb7c2334305a8a066183cd
jsferrer1 <jsferrer1@yahoo.com>
2020-07-17 17:17:28 +0800

    Adds the main route and controller

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { mainRoutes } from "./routes/MainRoutes";
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  private config(): void {
+    // support application/json
+    this.app.use(bodyParser.json());
+    //support application/x-www-form-urlencoded post data
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    // Routing
+    this.app.use("/", mainRoutes);
+  }
+}
+
+export default new App().app;
