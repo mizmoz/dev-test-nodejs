93bb05d23ebc5ed6970c64628e9941bfe1cbeb1b
jsferrer1 <jsferrer1@yahoo.com>
2020-07-18 01:40:24 +0800

    Uses redis to persist the data

--- src/controllers/MainController.ts
+++ src/controllers/MainController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import Countries from "../api/country";
-
-// TODO: add random population
+const redisClient = require('../api/redis-client');
 
 export class MainController {
   public countries: any;
@@ -17,45 +16,50 @@ export class MainController {
   public async init() {
     try {
       console.log('Load the countries.');
-      let id = 1;
-      let cs = await (await Countries()).map(e => {
-        return ({id: id++, ...e, population: Math.floor(Math.random() * 1001)});
+      await (await Countries()).map(async function(e) {
+        //return ();
+        const value = {...e, population: Math.floor(Math.random() * 1001)};
+        await redisClient.setAsync(e.code, JSON.stringify(value));
       });
-      this.countries = cs;
+      //this.countries = cs;
     } catch(e) {
       console.log('init error: ', e);
     }
   }
 
-  public getCountries(req: Request, res: Response) {
+  public async getCountries(req: Request, res: Response) {
+    let keys = await redisClient.keysAsync('*');
+    let countries = await redisClient.mgetAsync(keys);
     res.status(200).send({
-      countries: this.countries
+      countries: countries
     });
   }
 
-  public getCountriesAscPop(req: Request, res: Response) {
-    let cs = this.countries;
-    cs.sort(function(a:any, b:any) {
+  public async getCountriesAscPop(req: Request, res: Response) {
+    let keys = await redisClient.keysAsync('*');
+    let countries = await redisClient.mgetAsync(keys);
+    await countries.sort(function(a:any, b:any) {
       return parseInt(a.population) - parseInt(b.population);
     });
     res.status(200).send({
-      countries: cs
+      countries: countries
     });
   }
 
-  public getCountriesDescPop(req: Request, res: Response) {
-    let cs = this.countries;
-    cs.sort(function(a:any, b:any) {
+  public async getCountriesDescPop(req: Request, res: Response) {
+    let keys = await redisClient.keysAsync('*');
+    let countries = await redisClient.mgetAsync(keys);
+    await countries.sort(function(a:any, b:any) {
       return parseInt(b.population) - parseInt(a.population);
     });
     res.status(200).send({
-      countries: cs
+      countries: countries
     });
   }
 
-  public getCountryById(req: Request, res: Response) {
-    let id = req.params.id;
-    let country = this.countries.find(function(x:any) { return x.id==id });
+  public async getCountryById(req: Request, res: Response) {
+    let key = req.params.id;
+    let country = await redisClient.getAsync(key);
     if (country) {
       res.status(200).send({
         country: country
@@ -67,12 +71,16 @@ export class MainController {
     }
   }
 
-  public updateCountryPop(req: Request, res: Response) {
-    let id = req.params.id;
+  public async updateCountryPop(req: Request, res: Response) {
+    let key = req.params.id;
     let pop = parseInt(req.body.population);
-    let country = this.countries.find(function(x:any) { return x.id==id });
-    if (country) {
+    let str = await redisClient.getAsync(key);
+    if (str) {
+      let country = JSON.parse(str);
       country.population = pop;
+
+      // update the database then send response
+      await redisClient.setAsync(key, JSON.stringify(country));
       res.status(200).send({
         message: 'Country population updated!'
       });
@@ -83,16 +91,20 @@ export class MainController {
     }
   }
 
-  public updateCountry(req: Request, res: Response) {
-    let id = req.params.id;
+  public async updateCountry(req: Request, res: Response) {
+    let key = req.params.id;
     let name = req.body.name;
     let code = req.body.code;
     let pop = parseInt(req.body.population);
-    let country = this.countries.find(function(x:any) { return x.id==id });
-    if (country) {
+    let str = await redisClient.getAsync(key);
+    if (str) {
+      let country = JSON.parse(str);
       country.name = name;
       country.code = code;
       country.population = pop;
+
+      // update the database then send response
+      await redisClient.setAsync(key, JSON.stringify(country));
       res.status(200).send({
         message: 'Country updated!'
       });
@@ -103,11 +115,10 @@ export class MainController {
     }
   }
 
-  public deleteCountryById(req: Request, res: Response) {
-    let id = req.params.id;
-    let cs = this.countries;
-    let item = cs.splice(cs.findIndex(function(e:any) {return e.id === id}), 1)
-    if (item.length > 0) {
+  public async deleteCountryById(req: Request, res: Response) {
+    let key = req.params.id;
+    let result = await redisClient.delAsync(key);
+    if (result > 0) {
       res.status(200).send({
         message: 'Country deleted!'
       });
