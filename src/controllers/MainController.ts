90be1c9e948687585aeafb90a4f2ec656c1ddc11
jsferrer1 <jsferrer1@yahoo.com>
2020-07-17 21:01:04 +0800

    Includes the update and delete functions

--- src/controllers/MainController.ts
+++ src/controllers/MainController.ts
@@ -11,11 +11,16 @@ export class MainController {
     this.init();
   }
 
+  /**
+   * init will include additional properties into the country: id and random population
+   */
+
   public async init() {
     try {
       console.log('init');
+      let id = 1;
       let cs = await (await Countries()).map(e => {
-        return ({...e, population: Math.floor(Math.random() * 1001)});
+        return ({id: id++, ...e, population: Math.floor(Math.random() * 1001)});
       });
       this.countries = cs;
     } catch(e) {
@@ -30,20 +35,95 @@ export class MainController {
   }
 
   public getCountries(req: Request, res: Response) {
-    //let c : typeof Countries = [];
     res.status(200).send({
       countries: this.countries
     });
   }
 
-  public async getCountriesSorted(req: Request, res: Response) {
-    //let c : typeof Countries = [];
-    let countries = await Countries();
+  public getCountriesAscPop(req: Request, res: Response) {
+    let cs = this.countries;
+    cs.sort(function(a:any, b:any) {
+      return parseInt(a.population) - parseInt(b.population);
+    });
     res.status(200).send({
-      countries: countries
+      countries: cs
     });
   }
 
+  public getCountriesDescPop(req: Request, res: Response) {
+    let cs = this.countries;
+    cs.sort(function(a:any, b:any) {
+      return parseInt(b.population) - parseInt(a.population);
+    });
+    res.status(200).send({
+      countries: cs
+    });
+  }
+
+  public getCountryById(req: Request, res: Response) {
+    let id = req.params.id;
+    let country = this.countries.find(function(x:any) { return x.id==id });
+    if (country) {
+      res.status(200).send({
+        country: country
+      });
+    } else {
+      res.status(404).send({
+        message: 'Country code not found!'
+      });
+    }
+  }
+
+  public updateCountryPop(req: Request, res: Response) {
+    let id = req.params.id;
+    let pop = parseInt(req.body.population);
+    let country = this.countries.find(function(x:any) { return x.id==id });
+    if (country) {
+      country.population = pop;
+      res.status(200).send({
+        message: 'Country population updated!'
+      });
+    } else {
+      res.status(404).send({
+        message: 'Country code not found!'
+      });
+    }
+  }
+
+  public updateCountry(req: Request, res: Response) {
+    let id = req.params.id;
+    let name = req.body.name;
+    let code = req.body.code;
+    let pop = parseInt(req.body.population);
+    let country = this.countries.find(function(x:any) { return x.id==id });
+    if (country) {
+      country.name = name;
+      country.code = code;
+      country.population = pop;
+      res.status(200).send({
+        message: 'Country updated!'
+      });
+    } else {
+      res.status(404).send({
+        message: 'Country not found!'
+      });
+    }
+  }
+
+  public deleteCountryById(req: Request, res: Response) {
+    let id = req.params.id;
+    let cs = this.countries;
+    let item = cs.splice(cs.findIndex(function(e:any) {return e.id === id}), 1)
+    if (item.length > 0) {
+      res.status(200).send({
+        message: 'Country deleted!'
+      });
+    } else {
+      res.status(404).send({
+        message: 'Country not found!'
+      });
+    }
+  }
 }
 
 export const mainController = new MainController();
--- src/routes/MainRoutes.ts
+++ src/routes/MainRoutes.ts
@@ -12,9 +12,35 @@ class MainRoutes {
     this.router.get("/", (req: express.Request, res: express.Response) =>
       mainController.root(req, res)
     );
+
     this.router.get("/countries", (req: express.Request, res: express.Response) =>
       mainController.getCountries(req, res)
     );
+
+    this.router.get("/countries/pop/asc", (req: express.Request, res: express.Response) =>
+      mainController.getCountriesAscPop(req, res)
+    );
+
+    this.router.get("/countries/pop/desc", (req: express.Request, res: express.Response) =>
+      mainController.getCountriesDescPop(req, res)
+    );
+
+    this.router.get("/country/:id", (req: express.Request, res: express.Response) =>
+      mainController.getCountryById(req, res)
+    );
+
+    this.router.put("/country/:id/pop", (req: express.Request, res: express.Response) =>
+      mainController.updateCountryPop(req, res)
+    );
+
+    this.router.put("/country/:id", (req: express.Request, res: express.Response) =>
+      mainController.updateCountry(req, res)
+    );
+
+    this.router.delete("/country/:id", (req: express.Request, res: express.Response) =>
+      mainController.deleteCountryById(req, res)
+    );
+
   }
 }
 
