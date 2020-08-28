3f73e1ee57cda177298b53591e57cca0f9444bfb
Jeric Bas <jericrayebas@gmail.com>
2020-08-28 13:37:47 +0800

    fixed redis issue

--- src/redis.ts
+++ src/redis.ts
@@ -1,16 +1,9 @@
-// const Redis = require('ioredis')
-// const redis = new Redis();
-
-// // const client = redis.createClient({
-// //     host: 'redis-server',
-// //     port: 6379// env
-// // })
-
-// export default redis;
-
 const redis = require("redis");
 const { promisify } = require("util");
-const client = redis.createClient();
+const client = redis.createClient({
+      host: 'redis',
+    port: 6379// env
+});
 
 export default {
   ...client,
