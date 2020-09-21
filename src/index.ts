47a69706d575d7f32f46d1c41efb4492b4464e77
gekkeiju <juanlorenzo.villegas@gmail.com>
2020-09-21 16:19:01 +0800

    testing typescript

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import restify from 'restify'
+import {Request, Response, Next} from 'restify'
 import corsMiddleware from 'restify-cors-middleware'
+import country from './api/country'
 
 const server = restify.createServer()
 
@@ -11,3 +13,29 @@ const cors = corsMiddleware({
 })
 server.pre(cors.preflight)
 server.use(cors.actual)
+
+const getAll =
+    (req: Request, res: Response, next: Next) => 
+        country()
+        .then(countries => {
+            res.send(countries)
+            next()
+        })
+        .catch(err => {
+            next(new Error('Something went wrong.'))
+        })
+
+server.get('/countries', getAll)
+
+server.listen(7000, () => {
+    console.log('%s listening at %s', 'test', 'test');
+})
+
+/*
+* Graceful termination */
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Process terminated.')
+        process.exit(0)
+    })
+})
