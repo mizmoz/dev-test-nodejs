b2f455ee0d3bb9931812cccdf80c3c49a82d5425
gekkeiju <juanlorenzo.villegas@gmail.com>
2020-09-21 19:24:42 +0800

    update routes

--- src/api/route/index.ts
+++ src/api/route/index.ts
@@ -6,5 +6,8 @@ export default (server: Server) => {
     server.get('/src/api/country', number2)
 
     server.get('/:route', generic.readAll)
+    server.get('/:route/:id', generic.read)
     server.post('/:route', generic.create)
+    server.put('/:route/:id', generic.update)
+    server.del('/:route/:id', generic.delete)
 }
